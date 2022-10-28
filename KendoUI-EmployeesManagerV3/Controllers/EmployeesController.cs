9d0d3fa3f77e22d1924b0c764d3f699ba8f47375
Huyen2402 <huyenb1910384@student.ctu.edu.vn>
2022-10-28 19:17:43 +0700

    qua punnnnnnn

--- KendoUI-EmployeesManagerV3/Controllers/EmployeesController.cs
+++ KendoUI-EmployeesManagerV3/Controllers/EmployeesController.cs
@@ -4,8 +4,11 @@ using KendoUI_EmployeesManagerV3.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Http.Results;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace KendoUI_EmployeesManagerV3.Controllers
 {
@@ -21,6 +24,45 @@ namespace KendoUI_EmployeesManagerV3.Controllers
         {
             List<Employees> list = db.Employees.ToList();
             return Json(list.ToDataSourceResult(req), JsonRequestBehavior.AllowGet);
+        }
+
+        
+        //public ActionResult Delete([DataSourceRequest] DataSourceRequest req , Employees e)
+        //{
+        //    if(e == null)
+        //    {
+        //        Response.StatusCode = 404;
+        //    }
+        //    else
+        //    {
+        //        Employees User = db.Employees.SingleOrDefault(n => n.Id == e.Id);
+        //        if(User == null)
+        //        {
+        //            Response.StatusCode = 404;
+        //        }
+        //        else
+        //        {
+        //            db.Employees.Remove(User);
+        //            db.SaveChanges();
+                   
+        //        }
+               
+        //    }
+        //   return View();
+        //}
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Delete(Employees product)
+        {
+            RouteValueDictionary routeValues;
+
+            //Delete the record
+            db.Employees.Remove(product);
+
+            routeValues = this.GridRouteValues();
+
+            //Redisplay the grid
+            return RedirectToAction("Index", routeValues);
         }
     }
 }
