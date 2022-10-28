6da575e2a78a9ed6e4d1e31340784f0496e9b69e
Huyen2402 <huyenb1910384@student.ctu.edu.vn>
2022-10-28 17:54:25 +0700

    Add project files.

---
+++ KendoUI-EmployeesManagerV3/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using KendoUI_EmployeesManagerV3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUI_EmployeesManagerV3.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        ManageEmployeesTestEntities db = new ManageEmployeesTestEntities();
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult getList([DataSourceRequest] DataSourceRequest req)
+        {
+            List<Employees> list = db.Employees.ToList();
+            return Json(list.ToDataSourceResult(req), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
